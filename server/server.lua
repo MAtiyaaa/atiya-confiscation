132f9ded9038555d432b460d6ab11a75e5c873c1
Atiya <45663878+MAtiyaaa@users.noreply.github.com>
2023-12-03 21:10:51 +0400

    Updated server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,17 +1,53 @@
 QBCore = exports['qb-core']:GetCoreObject()
 
 local lockers = {}
+local lockerTimers = {}
+
+local function saveLockerTimes()
+    local timeData = {}
+    for cid, timeLocked in pairs(lockerTimers) do
+        timeData[cid] = timeLocked
+    end
+
+    local file = io.open('lockerTimes.json', 'w')
+    if file then
+        file:write(json.encode(timeData))
+        file:close()
+    end
+end
+
+
+local function loadLockerTimes()
+    local file = io.open('lockerTimes.json', 'r')
+    if file then
+        local data = file:read('*a')
+        file:close()
+        lockerTimers = json.decode(data) or {}
+    end
+end
+
+AddEventHandler('onResourceStart', function(resourceName)
+    if GetCurrentResourceName() == resourceName then
+        loadLockerTimes()
+    end
+end)
+
 local function getLocker(citizenId)
     return lockers[citizenId]
 end
 
-local function hasAccess(player)
+local function hasAccess(player, citizenId)
     if Config.AccessControl.checkType == 'job' then
         return player.PlayerData.job.name == Config.AccessControl.jobName
     elseif Config.AccessControl.checkType == 'type' then
         return player.PlayerData.job.type == Config.AccessControl.jobName
     end
-    return false
+
+    if lockerTimers[citizenId] and lockerTimers[citizenId] > os.time() then
+        return false
+    end
+
+    return true
 end
 
 local function createLocker(citizenId)
@@ -30,32 +66,77 @@ local function createLocker(citizenId)
     return locker
 end
 
-QBCore.Commands.Add(Config.Command.name, Config.Command.description, {{name = Config.Command.usage, help = Config.Command.help}}, true, function(source, args)
+QBCore.Commands.Add(Config.Commands.openLocker.name, Config.Commands.openLocker.description, {{name = Config.Commands.openLocker.usage, help = 'Enter Player ID or CID'}}, true, function(source, args)   
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
-    if Player and hasAccess(Player) then
-        local targetId = args[1]
-        if targetId then
-            local targetPlayer = QBCore.Functions.GetPlayerByCitizenId(targetId) or QBCore.Functions.GetPlayer(tonumber(targetId))
-            if targetPlayer then
-                local citizenId = targetPlayer.PlayerData.citizenid
-                local locker = createLocker(citizenId)
+    local targetId = args[1]
+
+    if Player and hasAccess(Player, targetId) then
+        local targetPlayer = QBCore.Functions.GetPlayerByCitizenId(targetId) or QBCore.Functions.GetPlayer(tonumber(targetId))
+        if targetPlayer then
+            local citizenId = targetPlayer.PlayerData.citizenid
+            local locker = createLocker(citizenId)
+            if not lockerTimers[citizenId] or lockerTimers[citizenId] <= os.time() then
                 exports.ox_inventory:forceOpenInventory(src, 'stash', locker.id)
             else
-                TriggerClientEvent('QBCore:Notify', src, 'Player not found', 'error')
+                TriggerClientEvent('QBCore:Notify', src, 'This locker is currently locked.', 'error')
             end
         else
-            TriggerClientEvent('QBCore:Notify', src, 'No player ID provided', 'error')
+            TriggerClientEvent('QBCore:Notify', src, 'Player not found', 'error')
         end
     else
-        TriggerClientEvent('QBCore:Notify', src, 'You are not a police officer!', 'error')
+        TriggerClientEvent('QBCore:Notify', src, 'You are not authorized!', 'error')
     end
 end, false)
+QBCore.Commands.Add(Config.Commands.lockLocker.name, Config.Commands.lockLocker.description, {
+    {name = 'ID/CitizenID', help = 'Enter the player\'s server ID or CitizenID'},
+    {name = 'Minutes', help = 'How many minutes should the locker stay locked'}
+    }, true, function(source, args)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    local targetId = args[1]
+    local lockDuration = tonumber(args[2])
+
+    if Player and hasAccess(Player, targetId) then
+        local targetPlayer = QBCore.Functions.GetPlayerByCitizenId(targetId) or QBCore.Functions.GetPlayer(tonumber(targetId))
+        if targetPlayer and lockDuration and lockDuration > 0 then
+            local citizenId = targetPlayer.PlayerData.citizenid
+            lockerTimers[citizenId] = os.time() + (lockDuration * 60)
+            saveLockerTimes()          
+            TriggerClientEvent('QBCore:Notify', src, 'Locker is locked for ' .. lockDuration .. ' minutes', 'success')
+        else
+            TriggerClientEvent('QBCore:Notify', src, 'Invalid parameters', 'error')
+        end
+    else
+        TriggerClientEvent('QBCore:Notify', src, 'You are not authorized!', 'error')
+    end
+end, false)
+
+RegisterNetEvent('qb-policelockers:server:CheckLockerStatus', function(citizenId)
+    local src = source
+    local locker = createLocker(citizenId)
+    if locker then
+        if lockerTimers[citizenId] and lockerTimers[citizenId] > os.time() then
+            local remainingTime = math.ceil((lockerTimers[citizenId] - os.time()) / 60)
+            TriggerClientEvent('qb-policelockers:client:LockerStatus', src, true, remainingTime)
+        else
+            exports.ox_inventory:forceOpenInventory(src, 'stash', locker.id)
+        end
+    else
+        TriggerClientEvent('QBCore:Notify', src, 'Your locker is empty', 'error')
+    end
+end)
 
 
 RegisterNetEvent('qb-policelockers:server:OpenLocker', function(citizenId)
     local src = source
-    local locker = getLocker(citizenId)
+    if lockerTimers[citizenId] and lockerTimers[citizenId] > os.time() then
+        local remainingTime = math.ceil((lockerTimers[citizenId] - os.time()) / 60)
+        TriggerClientEvent('qb-policelockers:client:LockerStatus', src, true, remainingTime)
+        return
+    end
+
+    local locker = createLocker(citizenId)
     if locker then
         exports.ox_inventory:forceOpenInventory(src, 'stash', locker.id)
     else
