078d02b28865de1df3356d09945b2cbc88be880e
Atiya <45663878+MAtiyaaa@users.noreply.github.com>
2023-12-03 21:11:52 +0400

    Updated fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,14 +3,14 @@ game 'gta5'
 
 author 'Atiya'
 description 'Locker System for Police'
-version '1.0.0'
+version '1.0.1'
 
 shared_script 'config.lua'
 client_script 'client/client.lua'
 server_script 'server/server.lua'
 
+
 dependencies {
     'yarn',
-    'ox_inventory',
-    'qb-policejob'
+    'ox_inventory'
   }
