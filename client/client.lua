70eb4699b4335c67829244a77aa90aa33be7dba2
Atiya <45663878+MAtiyaaa@users.noreply.github.com>
2023-12-04 18:43:47 +0400

    Ped animation and prop added

--- client/client.lua
+++ client/client.lua
@@ -13,15 +13,34 @@ Citizen.CreateThread(function()
         SetBlockingOfNonTemporaryEvents(ped, true)
         FreezeEntityPosition(ped, true)
 
+        if pedConfig.animDict and pedConfig.animName then
+            RequestAnimDict(pedConfig.animDict)
+            while not HasAnimDictLoaded(pedConfig.animDict) do
+                Wait(1)
+            end
+            TaskPlayAnim(ped, pedConfig.animDict, pedConfig.animName, 8.0, -8.0, -1, 1, 0, false, false, false)
+        end
+        
+        if pedConfig.prop then
+            local propHash = GetHashKey(pedConfig.prop)
+            RequestModel(propHash)
+            while not HasModelLoaded(propHash) do
+                Wait(1)
+            end
+
+            local prop = CreateObject(propHash, 0, 0, 0, true, true, true)
+            AttachEntityToEntity(prop, ped, GetPedBoneIndex(ped, pedConfig.propBone), pedConfig.propPlacement.x, pedConfig.propPlacement.y, pedConfig.propPlacement.z, pedConfig.propRotation.x, pedConfig.propRotation.y, pedConfig.propRotation.z, true, true, false, true, 1, true)
+        end
+
         local interactionOptions = {
             {
                 event = 'qb-policelockers:client:RequestOpenLocker',
-                icon = 'fas fa-box-open',
+                icon = 'fas fa-lock',
                 label = 'Open Locker',
             }
         }
 
-        if pedConfig.useQbTarget then
+        if Config.QBTarget then
             exports['qb-target']:AddTargetEntity(ped, {
                 options = interactionOptions,
                 distance = 2.0
@@ -38,6 +57,7 @@ Citizen.CreateThread(function()
         end
     end
 end)
+
 
 RegisterNetEvent('qb-policelockers:client:RequestOpenLocker', function()
     local Player = QBCore.Functions.GetPlayerData()
