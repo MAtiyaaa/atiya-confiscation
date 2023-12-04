33481f841d6448b73c27ccbc9c19cc2781aa18a1
Atiya <45663878+MAtiyaaa@users.noreply.github.com>
2023-12-04 18:42:58 +0400

    Animation and prop to peds, multi-job support

--- config.lua
+++ config.lua
@@ -12,16 +12,42 @@ Config.Commands = {
     }
 }
 
+Config.QBTarget = true -- Set to true to use qb-target, false to use ox_target
+
 Config.Peds = {
     {
-        model = 's_m_y_cop_01', -- Default cop ped model
+        model = 's_m_y_cop_01', -- Get ped names from https://wiki.rage.mp/index.php?title=Peds
         location = vector4(442.39, -981.911, 29.69, 85.0), -- GABZ MRPD
-        useQbTarget = true -- Set to true to use qb-target, false to use ox_target
+
+        animDict = 'missheistdockssetup1clipboard@base',  -- Get animations from https://alexguirre.github.io/animations-list/
+        animName = 'base',  -- Get animations from https://alexguirre.github.io/animations-list/
+
+        prop = 'prop_notepad_01',  -- Find Prop here https://gist.github.com/leonardosnt/53faac01a38fc94505e9
+        propBone = 18905, -- Find Bones here https://wiki.rage.mp/index.php?title=Bones
+        propPlacement = vector3(0.1, 0.02, 0.05),
+        propRotation = vector3(10.0, 0.0, 0.0) 
+    },    
+    {
+        model = 'csb_cop',
+        location = vector4(1844.57, 2581.75, 45.01, 76.00), -- GABZ BOILINGBROKE PEN.
+
+        animDict = 'missheistdockssetup1clipboard@base',
+        animName = 'base',
+        
+        prop = 'prop_notepad_01',
+        propBone = 18905,
+        propPlacement = vector3(0.1, 0.02, 0.05),
+        propRotation = vector3(10.0, 0.0, 0.0) 
     },
 }
 
 Config.AccessControl = {
-    jobName = 'police',  -- Change the name of the job/type that can access the lockers (like 'police' or 'sheriff' or 'leo')
+    jobName = { 
+        'police',  -- Change the name of the job/type that can access the lockers (like 'police' or 'sheriff' or 'leo')
+        -- 'sherrif',
+        -- 'leo',
+        -- 'bcso'
+    },    
     checkType = 'job',   -- 'job' for jobName, 'type' for jobType (if you're using jobtype = 'leo' due to multiple departments, set to 'type', if only using the default police job, set to 'job')
 }
 
