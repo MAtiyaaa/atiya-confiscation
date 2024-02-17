c5abff9378794f7fdb0b8dd039aebdcc6697252b
Atiya <45663878+MAtiyaaa@users.noreply.github.com>
2024-02-17 18:09:49 +0400

    3D Text Support

--- client/client.lua
+++ client/client.lua
@@ -1,13 +1,14 @@
 QBCore = exports['qb-core']:GetCoreObject()
 
 Citizen.CreateThread(function()
+    
     for _, pedConfig in pairs(Config.Peds) do
         local pedModel = GetHashKey(pedConfig.model)
         RequestModel(pedModel)
         while not HasModelLoaded(pedModel) do
             Wait(1)
         end
-
+        
         local ped = CreatePed(4, pedModel, pedConfig.location.x, pedConfig.location.y, pedConfig.location.z, pedConfig.location.w, false, true)
         SetEntityInvincible(ped, true)
         SetBlockingOfNonTemporaryEvents(ped, true)
@@ -39,13 +40,13 @@ Citizen.CreateThread(function()
                 label = 'Open Locker',
             }
         }
-
-        if Config.QBTarget then
+        
+        if Config.Target == 'QB' then
             exports['qb-target']:AddTargetEntity(ped, {
                 options = interactionOptions,
                 distance = 2.0
             })
-        else
+        elseif Config.Target == 'OX' then
             exports['ox_target']:AddEntityZone('policelocker_' .. pedConfig.model, ped, {
                 name = 'policelocker_' .. pedConfig.model,
                 debugPoly = false,
@@ -54,6 +55,23 @@ Citizen.CreateThread(function()
                 options = interactionOptions,
                 distance = 2.0
             })
+        elseif Config.Target == "3D" then
+            Citizen.CreateThread(function()
+                while true do
+                    Citizen.Wait(0)
+                    local playerCoords = GetEntityCoords(PlayerPedId())
+                    for _, v in pairs(Config.Peds) do
+                        local distance = #(playerCoords - vector3(v.location.x, v.location.y, v.location.z))
+                        
+                        if distance < 2.5 then
+                            DrawText3Ds(v.location.x, v.location.y, v.location.z + 1.0, "Press ~r~[E]~s~ To Open ~y~Locker~s~")
+                            if IsControlJustReleased(0, 38) then
+                                TriggerEvent('qb-policelockers:client:RequestOpenLocker')
+                            end
+                        end
+                    end
+                end
+            end)
         end
     end
 end)
@@ -71,3 +89,17 @@ RegisterNetEvent('qb-policelockers:client:LockerStatus', function(isLocked, rema
         TriggerServerEvent('qb-policelockers:server:OpenLocker', QBCore.Functions.GetPlayerData().citizenid)
     end
 end)
+
+function DrawText3Ds(x, y, z, text)
+    local onScreen, _x, _y = World3dToScreen2d(x, y, z)
+    SetTextScale(0.32, 0.32)
+    SetTextFont(4)
+    SetTextProportional(1)
+    SetTextColour(255, 255, 255, 255)
+    SetTextEntry("STRING")
+    SetTextCentre(1)
+    AddTextComponentString(text)
+    DrawText(_x, _y)
+    local factor = (string.len(text)) / 500
+    DrawRect(_x, _y + 0.0125, 0.015 + factor, 0.03, 0, 0, 0, 80)
+end
