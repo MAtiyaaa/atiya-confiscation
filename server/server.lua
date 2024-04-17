f131905879991ce3be779e1231527d5bf5a72a82
Atiya <45663878+MAtiyaaa@users.noreply.github.com>
2024-04-17 17:36:31 +0400

    Auto-confiscation, ox_inventory fix

--- server/server.lua
+++ server/server.lua
@@ -62,7 +62,7 @@ local function createLocker(citizenId)
         local lockerId = 'locker-' .. citizenId
         local lockerLabel = 'Locker - ' .. citizenId
         local slots = Config.Locker.slots
-        local maxWeight = Config.Locker.maxWeight
+        local maxWeight = Config.Locker.weight
         locker = { id = lockerId, label = lockerLabel, slots = slots, weight = maxWeight, owner = citizenId }
         lockers[citizenId] = locker
 
@@ -89,7 +89,7 @@ QBCore.Commands.Add(Config.Commands.openLocker.name, Config.Commands.openLocker.
             local locker = createLocker(citizenId)
             if not lockerTimers[citizenId] or lockerTimers[citizenId] <= os.time() then
                 if Config.Inventory == 'QB' then
-                    TriggerClientEvent('qb-policelockers:client:open-locker-custom', src, locker.id)
+                    TriggerClientEvent('qb-confiscation:client:open-locker-custom', src, locker.id)
                 elseif Config.Inventory == 'OX' then
                     exports.ox_inventory:forceOpenInventory(src, 'stash', locker.id)
                 end
@@ -152,16 +152,16 @@ QBCore.Commands.Add(Config.Commands.unlockLocker.name, Config.Commands.unlockLoc
     end
 end, false)
 
-RegisterNetEvent('qb-policelockers:server:CheckLockerStatus', function(citizenId)
+RegisterNetEvent('qb-confiscation:server:CheckLockerStatus', function(citizenId)
     local src = source
     local locker = createLocker(citizenId)
     if locker then
         if lockerTimers[citizenId] and lockerTimers[citizenId] > os.time() then
             local remainingTime = math.ceil((lockerTimers[citizenId] - os.time()) / 60)
-            TriggerClientEvent('qb-policelockers:client:LockerStatus', src, true, remainingTime)
+            TriggerClientEvent('qb-confiscation:client:LockerStatus', src, true, remainingTime)
         else
             if Config.Inventory == 'QB' then
-                TriggerClientEvent('qb-policelockers:client:open-locker-custom', src, locker.id)
+                TriggerClientEvent('qb-confiscation:client:open-locker-custom', src, locker.id)
             elseif Config.Inventory == 'OX' then
                 exports.ox_inventory:forceOpenInventory(src, 'stash', locker.id)
             end
@@ -171,18 +171,66 @@ RegisterNetEvent('qb-policelockers:server:CheckLockerStatus', function(citizenId
     end
 end)
 
+AddEventHandler('qb-confiscation:confiscateItems', function(playerId)
+    local Player = QBCore.Functions.GetPlayer(playerId)
+    if Player then
+        local lockerId = 'locker-' .. Player.PlayerData.citizenid
+        local lockerInventory = exports.ox_inventory:GetInventory(lockerId)
+        local itemsProcessed = false
 
-RegisterNetEvent('qb-policelockers:server:OpenLocker', function(citizenId)
+        for _, itemSlot in pairs(Player.PlayerData.items) do
+            if itemSlot.name and itemSlot.count and itemSlot.count > 0 then
+                local item = string.lower(itemSlot.name)
+                local amount = itemSlot.count
+                local isInList = table.contains(Config.Confiscation.Items, item)
+
+                local shouldProcess = (Config.Confiscation.Mode == 'blacklist' and isInList) or
+                                      (Config.Confiscation.Mode == 'whitelist' and not isInList)
+
+                if shouldProcess then
+                    local canCarry = lockerInventory and exports.ox_inventory:CanCarryItem(lockerInventory, itemSlot.name, amount)
+                    if canCarry then
+                        itemsProcessed = true
+                        Player.Functions.RemoveItem(itemSlot.name, amount)
+                        AddToLocker(Player.PlayerData.citizenid, itemSlot.name, amount)
+                    else
+                        print("Not enough space in the locker to add " .. itemSlot.name)
+                        TriggerClientEvent('QBCore:Notify', playerId, "Not enough space in your locker for " .. itemSlot.name, 'error')
+                    end
+                end
+            else
+                print("Invalid item data or count for item: " .. tostring(itemSlot.name))
+            end
+        end
+
+        if itemsProcessed then
+            TriggerClientEvent('QBCore:Notify', playerId, 'All your items have been placed in the police locker', 'error')
+        else
+            -- TriggerClientEvent('QBCore:Notify', playerId, 'No items needed to be moved to the locker.', 'error')
+        end
+    end
+end)
+
+function table.contains(table, element)
+    for _, value in pairs(table) do
+        if string.lower(value) == element then
+            return true
+        end
+    end
+    return false
+end
+
+RegisterNetEvent('qb-confiscation:server:OpenLocker', function(citizenId)
     local src = source
     if lockerTimers[citizenId] and lockerTimers[citizenId] > os.time() then
         local remainingTime = math.ceil((lockerTimers[citizenId] - os.time()) / 60)
-        TriggerClientEvent('qb-policelockers:client:LockerStatus', src, true, remainingTime)
+        TriggerClientEvent('qb-confiscation:client:LockerStatus', src, true, remainingTime)
         return
     end
     local locker = createLocker(citizenId)
     if locker then
         if Config.Inventory == 'QB' then
-            TriggerClientEvent('qb-policelockers:client:open-locker-custom', src, locker.id)
+            TriggerClientEvent('qb-confiscation:client:open-locker-custom', src, locker.id)
         elseif Config.Inventory == 'OX' then
             exports.ox_inventory:forceOpenInventory(src, 'stash', locker.id)
         end
@@ -190,3 +238,25 @@ RegisterNetEvent('qb-policelockers:server:OpenLocker', function(citizenId)
         TriggerClientEvent('QBCore:Notify', src, 'Your locker is empty', 'error')
     end
 end)
+
+function AddToLocker(citizenId, item, amount, metadata)
+    local lockerId = 'locker-' .. citizenId
+    local lockerLabel = 'Locker - ' .. citizenId
+
+    local lockerInventory = exports.ox_inventory:GetInventory(lockerId, citizenId)
+    if not lockerInventory then
+        local slots = Config.Locker.slots
+        local maxWeight = Config.Locker.weight
+        exports.ox_inventory:RegisterStash(lockerId, lockerLabel, slots, maxWeight, citizenId)
+        lockerInventory = { id = lockerId, owner = citizenId }
+    end
+
+    if not exports.ox_inventory:CanCarryItem(lockerInventory, item, amount) then
+        print('Locker cannot carry more items')
+        return
+    end
+    local success, response = exports.ox_inventory:AddItem(lockerId, item, amount)
+    if not success then
+        print('Failed to add item to locker:', response)
+    end
+end
