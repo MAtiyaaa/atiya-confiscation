3621d4c0511b06cbac1eff02054a89e2a2915b4a
Atiya <45663878+MAtiyaaa@users.noreply.github.com>
2024-04-19 04:27:42 +0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,8 +2,8 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'Atiya'
-description 'Locker Confiscation System for Police'
-version '2.0.1'
+description 'Locker Confiscation System'
+version '2.0.2'
 
 shared_script 'config.lua'
 client_script 'client/*.lua'
