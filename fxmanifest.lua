cf1ce86df9209d2dbf5aba8098a6262fa38a4ba4
Atiya <matiyaaaa@gmail.com>
2024-04-28 02:28:56 +0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,13 +3,17 @@ game 'gta5'
 
 author 'Atiya'
 description 'Locker Confiscation System'
-version '2.0.3'
+version '2.0.4'
 
-shared_script 'config.lua'
+shared_scripts {
+  '@ox_lib/init.lua',
+  'config.lua'
+}
 client_script 'client/*.lua'
 server_script 'server/*.lua'
-
 
 dependencies {
     'yarn',
   }
+
+  lua54 'yes'
